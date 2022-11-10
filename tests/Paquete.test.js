52e220c68af0ab9ea4996d26a2191a67cb277b13
Isoldios <isoldi772@gmail.com>
2022-11-10 19:17:04 -0300

    TDD iteration

--- tests/Paquete.test.js
+++ tests/Paquete.test.js
@@ -4,3 +4,7 @@ test("Generar paquete",()=>{
     const paquete = new Paquete("Punto de destino 1");
     expect(paquete.destino).toBe("Punto de destino 1");
 });
+test("Etapa del paquete",()=>{
+    const paquete = new Paquete("Punto de destino 1");
+    expect(paquete.etapa()).toBe("Salida Local");
+});
