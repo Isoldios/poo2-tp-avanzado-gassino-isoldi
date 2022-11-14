f8bfd14d5577d7ff13f5dc64e2e1f0bb79121ac9
Isoldios <isoldi772@gmail.com>
2022-11-14 18:51:12 -0300

    TDD iteration

--- tests/Mapa.test.js
+++ tests/Mapa.test.js
@@ -1,12 +1,22 @@
 const Mapa=require("../src/Mapa")
 const Local=require("../src/Local")
 
-test("Generar mapa",()=>{
+beforeEach(()=>{
     mapa = new Mapa(1,2);
-    expect(mapa.dimensiones()).toStrictEqual([1,2]);
 });
 test("Generar mapa",()=>{
+    expect(mapa.dimensiones()).toStrictEqual([1,2]);
+});
+test("Agregar local al mapa",()=>{
     localA = new Local(0);
     mapa.agregarLocal(localA);
     expect(mapa.dimensiones()).toStrictEqual([2,2]);
+});
+test('Agregar 2 locales al mapa pero ninguna componente', () => {
+    expect(() => {
+    localA = new Local(0);
+    localB = new Local(1);
+    mapa.agregarLocal(localA);
+    mapa.agregarLocal(localB);
+    }).toThrow("No pueden haber mas locales que componentes");
 });
