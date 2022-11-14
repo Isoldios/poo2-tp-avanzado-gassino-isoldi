79aa9e0f61a39b7e2ea5f6c72b01c6ac79c645d8
Isoldios <isoldi772@gmail.com>
2022-11-14 18:18:12 -0300

    TDD iteration

--- src/Local.js
+++ src/Local.js
@@ -1,4 +1,12 @@
 function Local(paquetes){
     this.paquetes=paquetes;
+
+    this.paquetesXUniDeTiempo = function(){
+        if (this.paquetes>5){
+            throw new Error("No puede generar mas de 5 paquetes por unidad de tiempo");
+        }else{
+            return this.paquetes;
+        }
+    }
 }
 module.exports=Local;
--- tests/Local.test.js
+++ tests/Local.test.js
@@ -1,7 +1,8 @@
 const Local = require("../src/Local");
+
 test("Nuevo Local",()=>{
     local = new Local(3);
-    expect(local.paquetes).toBe(3);
+    expect(local.paquetesXUniDeTiempo()).toBe(3);
 });
 test('Genera mas de 5 paquetes por unidad de tiempo', () => {
     expect(() => {
