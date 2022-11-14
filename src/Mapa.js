f3716ef8dad806c152304bd0f44fb8a27340aef0
Isoldios <isoldi772@gmail.com>
2022-11-14 17:32:17 -0300

    TDD iteration

--- src/Mapa.js
+++ src/Mapa.js
@@ -3,7 +3,7 @@ function Mapa(filas,columnas){
     this.columnas=columnas;
 
     this.dimensiones = function(){
-        return this.filas+" filas, "+this.columnas+ " columnas";
+        return [this.filas,this.columnas];
     }
 }
 module.exports=Mapa;
--- tests/Mapa.test.js
+++ tests/Mapa.test.js
@@ -2,5 +2,5 @@ const Mapa=require("../src/Mapa")
 
 test("Generar mapa",()=>{
     mapa = new Mapa(1,2);
-    expect(mapa.dimensiones()).toBe("1 filas, 2 columnas");
+    expect(mapa.dimensiones()).toStrictEqual([1,2]);
 });
