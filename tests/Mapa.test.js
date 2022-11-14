9de868cfd04e02672dc4cb83c4a2e9294be3ecec
Isoldios <isoldi772@gmail.com>
2022-11-14 18:42:12 -0300

    TDD iteration

--- tests/Mapa.test.js
+++ tests/Mapa.test.js
@@ -1,6 +1,12 @@
 const Mapa=require("../src/Mapa")
+const Local=require("../src/Local")
 
 test("Generar mapa",()=>{
     mapa = new Mapa(1,2);
     expect(mapa.dimensiones()).toStrictEqual([1,2]);
+});
+test("Generar mapa",()=>{
+    localA = new Local(0);
+    mapa.agregarLocal(localA);
+    expect(mapa.dimensiones()).toStrictEqual([2,2]);
 });
