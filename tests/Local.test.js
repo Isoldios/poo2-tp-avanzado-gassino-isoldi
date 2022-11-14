6422c16215cf548be48a62d2ce1dbdd0548f98a5
Isoldios <isoldi772@gmail.com>
2022-11-14 18:16:57 -0300

    TDD iteration

--- tests/Local.test.js
+++ tests/Local.test.js
@@ -3,3 +3,9 @@ test("Nuevo Local",()=>{
     local = new Local(3);
     expect(local.paquetes).toBe(3);
 });
+test('Genera mas de 5 paquetes por unidad de tiempo', () => {
+    expect(() => {
+        local = new Local(6);
+        local.paquetesXUniDeTiempo();
+    }).toThrow("No puede generar mas de 5 paquetes por unidad de tiempo");
+});
