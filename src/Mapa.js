4574cd31dbc6fc78e4dcd94119da3e0bf9c4f4d9
Isoldios <isoldi772@gmail.com>
2022-11-15 15:43:11 -0300

    TDD iteration

--- src/Mapa.js
+++ src/Mapa.js
@@ -1,3 +1,5 @@
+const MapaModulo=require("./MapaModulo");
+
 function Mapa(filas,columnas){
     this.filas = filas;
     this.columnas=columnas;
@@ -5,15 +7,17 @@ function Mapa(filas,columnas){
     this.dimensiones = function(){
         return [this.filas,this.columnas];
     }
-    this.agregarLocal=function(local){
-        if (this.filas<this.columnas){
-            this.filas++;
-        }else if(this.filas==0 && this.columnas==0){
-            this.filas++;
-            this.columnas++;
-        }else{
-            throw new Error("No pueden haber mas locales que componentes");
-        }
+    this.mapaCompleto = function(){
+        let mapita=[];
+        let i=0;
+        MapaModulo.filas.forEach(local => {
+            mapita.push([local]);
+            MapaModulo.columnas.forEach(componente => {
+                mapita[i].push(componente)
+            });
+            i++;
+        });
+        return mapita;
     }
 }
 module.exports=Mapa;
--- tests/Mapa.test.js
+++ tests/Mapa.test.js
@@ -11,35 +11,16 @@ beforeEach(()=>{
 test("Generar mapa",()=>{
     expect(mapa.dimensiones()).toStrictEqual([0,0]);
 });
-test("Agregar local al mapa",()=>{
-    localA = new Local(5,0);
-    mapa.agregarLocal(localA);
-    expect(mapa.dimensiones()).toStrictEqual([1,1]);
-});
-test('Agregar 2 locales al mapa pero ninguna componente', () => {
-    expect(() => {
-    localA = new Local(5,0);
-    localB = new Local(5,1);
-    mapa.agregarLocal(localA);
-    mapa.agregarLocal(localB);
-    }).toThrow("No pueden haber mas locales que componentes");
-});
 test("Mostrar mapa como lista de listas de locales",()=>{
     localA = new Local(5,0);
     MapaModulo.agregarLocal(localA)
     expect(MapaModulo.filas).toStrictEqual([localA]);
 });
 test("Agregamos Facturacion",()=>{
-    localA = new Local(5,0);
-    MapaModulo.agregarLocal(localA);
     facturacionA=new Facturacion(6,0);
     MapaModulo.agregarComponente(facturacionA);
     expect(MapaModulo.columnas).toStrictEqual([facturacionA]);
 });
 test("Primer fila completa: Un local, una componente",()=>{
-    localA = new Local(5,0);
-    MapaModulo.agregarLocal(localA);
-    facturacionA=new Facturacion(6,0);
-    MapaModulo.agregarComponente(facturacionA);
-    expect(MapaModulo.mapaCompleto()).toStrictEqual([[localA,facturacionA]]);
+    expect(mapa.mapaCompleto()).toStrictEqual([[localA,facturacionA]]);
 });
