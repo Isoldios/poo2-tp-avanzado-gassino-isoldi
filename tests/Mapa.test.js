001aba7a4062ecb16bce7152ce0f7c37f41e0ccc
Isoldios <isoldi772@gmail.com>
2022-11-15 15:06:01 -0300

    TDD iteration

--- tests/Mapa.test.js
+++ tests/Mapa.test.js
@@ -1,5 +1,8 @@
 const Mapa=require("../src/Mapa")
 const Local=require("../src/Local")
+const Distribucion=require("../src/Distribucion")
+const Facturacion=require("../src/Facturacion")
+const Calidad=require("../src/Calidad")
 const MapaModulo=require("../src/MapaModulo");
 
 beforeEach(()=>{
@@ -9,20 +12,27 @@ test("Generar mapa",()=>{
     expect(mapa.dimensiones()).toStrictEqual([0,0]);
 });
 test("Agregar local al mapa",()=>{
-    localA = new Local(0);
+    localA = new Local(5,0);
     mapa.agregarLocal(localA);
     expect(mapa.dimensiones()).toStrictEqual([1,1]);
 });
 test('Agregar 2 locales al mapa pero ninguna componente', () => {
     expect(() => {
-    localA = new Local(0);
-    localB = new Local(1);
+    localA = new Local(5,0);
+    localB = new Local(5,1);
     mapa.agregarLocal(localA);
     mapa.agregarLocal(localB);
     }).toThrow("No pueden haber mas locales que componentes");
 });
 test("Mostrar mapa como lista de listas de locales",()=>{
-    localA = new Local(0);
+    localA = new Local(5,0);
     MapaModulo.agregarLocal(localA)
+    expect(MapaModulo.filas).toStrictEqual([localA]);
+});
+test("Agregamos Facturacion",()=>{
+    localA = new Local(5,0);
+    MapaModulo.agregarLocal(localA);
+    facturacionA=new Facturacion(6,0);
+    MapaModulo.agregarComponente(facturacionA);
     expect(MapaModulo.filas).toStrictEqual([localA]);
 });
